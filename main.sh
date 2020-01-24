1ff2baa82df5451f86b49c1bc0054e97cc3e9e34
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2020-01-24 18:54:18 +0000

    creating the repo

--- main.sh
+++ main.sh
@@ -1,1 +1,24 @@
-echo Hello World
+clear 
+echo '+------------------+'
+cal
+echo '+------------------+'
+date +'%d/%m/%Y %H:%M:%S'
+echo '+------------------+'
+echo
+
+# gcc -o sample sample.c
+# including any other gcc flags you like
+# ./path-to-sample/sample
+
+# intro=$PWD/"01 Preliminaries/01 Introduction"
+
+echo "building: "
+echo '+------------------+'
+echo "hello.c"
+echo '+------------------+'
+echo "binaries in ./Results"
+echo '+------------------+'
+
+gcc hello.c -o ./Results/hello
+objdump -D -M intel ./Results/hello | grep -A20 main.: > ./Results/results.txt
+cat ./Results/results.txt
